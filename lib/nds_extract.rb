15adfae23fa9d565557aefd1c5d8ee5144aebe2f
Kristine Du <kristine.a.du@gmail.com>
2020-02-04 05:35:47 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -24,7 +24,8 @@ def directors_totals(nds)
   result = {}
   index = 0
   while index < nds.length do
-    result[nds[index][:name]] = gross_for_director(nds[index])
+    director = nds[index]
+    result[director[:name]] = gross_for_director(director)
     index += 1
   end
   result
